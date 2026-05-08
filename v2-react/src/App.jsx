77840be19ccfe8ef1506533af93e1e5ea1915cc0
kevin git <ahlinjuniorkevincatarya@gmail.com>
2026-05-08 19:58:20 +0100

    feat: Initialize react project structure

---
+++ v2-react/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#0B0F19] text-white">
+      <h1 className="text-4xl font-bold">ENSAM CLOUD</h1>
+    </div>
+  );
+};
+
+export default App;
